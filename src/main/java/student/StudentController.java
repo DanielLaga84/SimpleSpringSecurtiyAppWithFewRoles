e0e2cc602e403b76f2728d39187a8d69a3bed715
daniellaga84@gmail.com <Erkano84!!>
2021-05-11 08:57:53 +0200

    initial commit with StudentController

---
+++ src/main/java/student/StudentController.java
@@ -0,0 +1,28 @@
+package student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1,"James Bond"),
+            new Student(2,"Daniel Laga"),
+            new Student(3,"Kozlowski kamil")
+    );
+
+    @GetMapping(path="{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId) {
+    return STUDENTS.stream()
+            .filter(e->studentId.equals(e.getStudentId())).findFirst().orElseThrow(()->new IllegalArgumentException("Student " + studentId + "does not exist"));
+    }
+}
