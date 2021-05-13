90ffc713fb52fee96d8135eba06e7d27f8d568c0
daniellaga84@gmail.com <Erkano84!!>
2021-05-13 11:53:47 +0200

    Make rememberMe() button in html and in ApplicationSecurityConfig.java.

--- src/main/java/com/example/studentsapi/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/studentsapi/security/ApplicationSecurityConfig.java
@@ -43,7 +43,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login").permitAll()
-                .defaultSuccessUrl("/courses",true);
+                .defaultSuccessUrl("/courses",true).and()
+                .rememberMe() ;// default 2 weeks;
 
 
     }
