7210186aa15f5e8f54a769749304559fb2cb59af
daniellaga84@gmail.com <Erkano84!!>
2021-05-13 12:07:56 +0200

    Logout() + logoutUrl + l

--- src/main/java/com/example/studentsapi/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/studentsapi/security/ApplicationSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
@@ -47,7 +48,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login").permitAll()
                 .defaultSuccessUrl("/courses",true).and()
                 .rememberMe().tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
-                .key("somethingverysecured");// default 2 weeks;
+                .key("somethingverysecured")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"))
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID", "remember-me" )
+                .logoutSuccessUrl("/login");// default 2 weeks;
 
 
     }
